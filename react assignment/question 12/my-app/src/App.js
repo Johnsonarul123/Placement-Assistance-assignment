24bc90b89cbba25ac1dfb817bba7edba32edbf12
[Arul Johnson] <[aruljohnson9411@gmail.com]>
2023-05-29 14:34:29 +0530

    added debouncing

--- react assignment/question 12/my-app/src/App.js
+++ react assignment/question 12/my-app/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [count,setCount] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Counter App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+
+        <h2>You clicked {count} times!</h2>
+        
       </header>
     </div>
   );
